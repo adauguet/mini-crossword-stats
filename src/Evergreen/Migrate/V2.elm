33070893ab97ac18d55dbbf7966aeda928c4928b
Antoine Dauguet <antoine@noredink.com>
2023-03-01 12:18:17 +0100

    Preparing for v2

--- src/Evergreen/Migrate/V2.elm
+++ src/Evergreen/Migrate/V2.elm
@@ -1,8 +1,8 @@
 module Evergreen.Migrate.V2 exposing (..)
 
-import Duration
 import Evergreen.V1.Record as Old
-import Evergreen.V1.Types as Old exposing (ToFrontend(..))
+import Evergreen.V1.Types as Old
+import Evergreen.V2.Duration as New
 import Evergreen.V2.Record as New
 import Evergreen.V2.Types as New
 import Lamdera.Migrations exposing (..)
@@ -24,10 +24,25 @@ frontendModel old =
 oldToNewRecord : Old.Record -> New.Record
 oldToNewRecord old =
     { id = old.id
-    , duration = Duration.fromFloat old.time
+    , duration = fromFloat old.time
     , date = old.date
     , players = old.players
     }
+
+
+fromFloat : Float -> New.Duration
+fromFloat float =
+    let
+        n =
+            truncate (float * 100)
+
+        minutes =
+            n // 100
+
+        seconds =
+            modBy 100 n
+    in
+    New.Duration <| minutes * 60 + seconds
 
 
 backendModel : Old.BackendModel -> ModelMigration New.BackendModel New.BackendMsg
@@ -62,7 +77,7 @@ frontendMsg old =
             MsgUnchanged
 
         Old.GotNow float posix ->
-            MsgMigrated ( New.GotNow (Duration.fromFloat float) posix, Cmd.none )
+            MsgMigrated ( New.GotNow (fromFloat float) posix, Cmd.none )
 
         Old.ClickedDelete _ ->
             MsgUnchanged
@@ -72,7 +87,7 @@ toBackend : Old.ToBackend -> MsgMigration New.ToBackend New.BackendMsg
 toBackend old =
     case old of
         Old.CreateNewRecord float posix setOfStrings ->
-            MsgMigrated ( New.CreateNewRecord (Duration.fromFloat float) posix setOfStrings, Cmd.none )
+            MsgMigrated ( New.CreateNewRecord (fromFloat float) posix setOfStrings, Cmd.none )
 
         Old.GetRecords ->
             MsgUnchanged
