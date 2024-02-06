7ba50b3d8179175ac889c552c5e1ccacf4f9f8be
Antoine Dauguet <antoine@noredink.com>
2024-02-06 15:58:37 +0100

    Add Kristen and Mariano.

--- src/Frontend.elm
+++ src/Frontend.elm
@@ -55,7 +55,7 @@ init : Url.Url -> Nav.Key -> ( Model, Cmd FrontendMsg )
 init _ key =
     let
         players =
-            [ "Antoine", "Eman", "Faraaz", "Ilias", "Mfon", "Waj" ]
+            [ "Antoine", "Eman", "Faraaz", "Ilias", "Kristen", "Mariano", "Mfon", "Waj" ]
     in
     ( { key = key
       , players = players
